85ca2d9d17009c9a46bd3fa8575b2ff8c6da47c9
Chibuzo Idia <caren.vhr@gmail.com>
2022-11-07 00:04:23 +0100

    Blog API

---
+++ Downloads/Telegram Desktop/basic_todo/Todo app/apps/forms.py
@@ -0,0 +1,11 @@
+#forms.py
+from pyexpat import model
+from django.forms import ModelForm
+from .models import*
+
+
+class Todo_Task_form(ModelForm):
+    class Meta:
+        model = TodoData
+        fields = ['name_of_task', 'task', 'created_by', 'done']
+        exclude = ['date_completed', 'date_started']
